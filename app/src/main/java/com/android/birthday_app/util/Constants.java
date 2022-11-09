228686441c232d1459b1e396c9d5d9ce85917e7d
Max-Diard <themax41@hotmail.fr>
2022-11-09 22:01:22 +0100

    Ajout de la constante Heroku

--- app/src/main/java/com/android/birthday_app/util/Constants.java
+++ app/src/main/java/com/android/birthday_app/util/Constants.java
@@ -5,7 +5,8 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import okhttp3.MediaType;
 
 public class Constants {
+    public static final String ROOT_API = "http://192.168.1.16:8080/api/v1";
 
-    public static final String ROOT_API = "https://frozen-thicket-99727.herokuapp.com/api/v1";
+//    public static final String ROOT_API = "https://frozen-thicket-99727.herokuapp.com/api/v1";
     public static final MediaType APPLICATION_JSON = MediaType.parse("application/json; charset=utf-8");
 }
